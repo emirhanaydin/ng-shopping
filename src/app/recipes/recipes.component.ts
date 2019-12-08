85c94bb83574788fd037d322d497413ff92101f7
emirhanaydin <emirhanaydin95@gmail.com>
2019-12-08 11:25:52 +0300

    Use Input and Output for the sub-component items

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe A', 'This is Recipe A', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Recipe B', 'This is Recipe B', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'),
+    new Recipe('Recipe C', 'This is Recipe C', 'https://cdn.pixabay.com/photo/2017/09/17/23/21/gastronomy-2760200_960_720.jpg'),
+  ];
+  selectedRecipe: Recipe;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipe = recipe;
   }
 
 }
